70c0bc24a46e06ce660f847cfb6c44a11f23107b
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-15 15:53:08 +0530

    Added Flat Cards Functionality in Project-1

--- components/FlatList.tsx
+++ components/FlatList.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { Component } from 'react'
 
 export class FlatList extends Component {
@@ -6,9 +6,18 @@ export class FlatList extends Component {
         return (
             <View>
                 <Text style={styles.headerText}>Flat Cards</Text>
-                <View>
-                    <View style={styles.cardComponent}>
-                        <Text>Red Card</Text>
+                <View style={styles.mainContainer}>
+                    <View style={[styles.cardComponent, styles.cardOne]}>
+                        <Text>Red </Text>
+                    </View>
+                    <View style={[styles.cardComponent, styles.cardTwo]}>
+                        <Text>Green </Text>
+                    </View>
+                    <View style={[styles.cardComponent, styles.cardThree]}>
+                        <Text>Blue </Text>
+                    </View>
+                    <View style={[styles.cardComponent, styles.cardFour]}>
+                        <Text>Purple </Text>
                     </View>
                 </View>
             </View>
@@ -20,8 +29,33 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold"
     },
+    mainContainer: {
+        marginTop: 10,
+        flex: 1,
+        flexDirection: "row",
+        marginInline: 8,
+        gap: 20
+    },
     cardComponent: {
-        backgroundColor: "red"
-    }
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        width: 120,
+        height: 120,
+        borderRadius: 9,
+
+    },
+    cardOne: {
+        backgroundColor: "rgba(227, 7, 44, 0.79)",
+    },
+    cardTwo: {
+        backgroundColor: "rgba(3, 237, 186, 0.67)",
+    },
+    cardThree: {
+        backgroundColor: "rgba(9, 99, 147, 0.58)",
+    },
+    cardFour: {
+        backgroundColor: "rgba(126, 20, 180, 0.83)",
+    },
 })
 export default FlatList
