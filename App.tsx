115b26fd7ca20004269b2110ead5713554822694
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-25 21:34:43 +0530

    Practice about Navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,7 @@
-
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import FlatList from "./components/FlatList";
-import ScrollCards from "./components/ScrollCards";
-import ImageCard from "./components/ImageCard";
-// import DeepLinking from "./components/DeepLinking";
-import TouchableOpacityDemo from "./components/TouchableOpacityDemo";
-import Gallery from "./components/Gallery";
-import ChipsInput from "./components/ChipsInput/ChipsInput";
-import ActivityIndicator from "./components/ActivityIndicator/ActivityIndicator";
-import ActivityIndicatorPractice from "./components/ActivityIndicator/ActivityIndicator";
-import { useState } from "react";
-import ModalPractice from "./components/Modal/ModalPractice";
-import StatusBarPractice from "./components/StatusBarPractice/StatusBarPractice";
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./components/Navigation/Home";
+import Login from "./components/Navigation/Login";
 
 /**
  * Sample React Native App
@@ -22,22 +11,14 @@ import StatusBarPractice from "./components/StatusBarPractice/StatusBarPractice"
  */
 function App() {
   // const [showData, setShpwData] = useState(false)
+  const Stack = createNativeStackNavigator()
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-
-        {/* <View style={{ flex: 1, alignItems: "center", justifyContent: "center", }}>
-          <TouchableOpacity
-            onPress={() => setShpwData(true)}>
-            <Text style={{ backgroundColor: "blue", color: "white", padding: 10 }}>Load Data</Text>
-          </TouchableOpacity>
-        </View>
-        {showData && <ActivityIndicatorPractice />} */}
-        {/* <ModalPractice /> */}
-        <StatusBarPractice />
-      </SafeAreaView>
-    </SafeAreaProvider>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ components/Navigation/Home.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+
+const Home = () => {
+    const navigation = useNavigation()
+    return (
+        <View style={styles.container}>
+            <Text>
+                Home Screen
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('login')}>
+                <Text style={styles.btn}>Route me to Login</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 20
+    },
+    btn: {
+        backgroundColor: "rgb(198, 26, 34)",
+        color: "white",
+        maxWidth: 100,
+        padding: 10,
+        borderRadius: 12
+    }
+})
